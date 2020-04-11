025814b5eebac6b9709e2826676ef734d7da39f4
Trumand <furtado.pedropaulo@gmail.com>
2020-04-11 18:33:16 -0300

    doing Icons

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import {FaGithubSquare, FaLinkedin, FaTwitterSquare, FaCloudDownloadAlt, FaLongArrowAltRight} from 'react-icons/fa'
 
 import ImgProfile from '../../assets/Pedro_profile_2.png'
+import Imgbackground from '../../assets/background.jpg'
+
 
 import * as S from './style'
 
@@ -10,16 +13,37 @@ export default function Profile() {
 
       <S.Section>
         <S.ContainerImage>
+          <img src={Imgbackground} alt="Background" style={{ width: "100%" }}/>
         </S.ContainerImage>
 
-        <S.ContainerImageProfile >
-          <img src={ImgProfile} alt="profile" style={{width: "100%", borderRadius:"50%"}} />
-        </S.ContainerImageProfile >
+        <S.ContainerDescription>
+          <S.ContainerImageProfile >
+            <img src={ImgProfile} alt="profile" style={{width: "100%", borderRadius:"50%"}} />
+          </S.ContainerImageProfile >
+            <S.H1>Pedro Furtado</S.H1>
+            <S.H4>Web Developer</S.H4>
+            <S.ContainerIcons>
+
+              <S.StyleIcons href="/">
+                  <FaGithubSquare size={25} />
+
+              </S.StyleIcons>
+
+              <S.StyleIcons href="/">
+                  <FaLinkedin size={25} />
+              </S.StyleIcons>
+
+              <S.StyleIcons href="/">
+                  <FaTwitterSquare size={25} /> 
+              </S.StyleIcons>
+
+            </S.ContainerIcons>
+        </S.ContainerDescription>
       </S.Section>
 
       <S.ContainerButtons>
-        <S.Buttons>DOWNLOAD CV</S.Buttons>
-        <S.Buttons>CONTACT ME</S.Buttons>
+        <S.Buttons> <S.Span>DOWNLOAD CV</S.Span> <FaCloudDownloadAlt size={15} /> </S.Buttons>
+        <S.Buttons><S.Span>CONTACT ME</S.Span> <FaLongArrowAltRight size={15} /> </S.Buttons>
       </S.ContainerButtons>
 
     </S.Container>
--- src/components/Profile/style.js
+++ src/components/Profile/style.js
@@ -25,19 +25,36 @@ export const ContainerImage = styled.div`
   background: pink;
 `
 export const ContainerImageProfile = styled.div`
-  width: 100px;
-  height: 100px;
-  background: green;
+  width: 102px;
+  height: 102px;
+  background: white;  
   border-radius: 50%;
   position: relative;
+  float: left;
   margin: 0 auto;
   position: relative;
-  top: -10%;
+  top: -25%;
 `
+export const H1 = styled.h1`
+  
+`
+
+export const H4 = styled.h4`
+  color: #78cc6d;
+`
+
 export const Image = styled.img`
-  width: 100%;
+  width: 100px;
   background-image: ${props => props.img};
   z-index: 2;
+`
+
+export const ContainerDescription = styled(ContainerImage)`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  background: white;
+  text-align: center;
 `
 
 export const ContainerButtons = styled.div`
@@ -49,5 +66,30 @@ export const ContainerButtons = styled.div`
 `
 
 export const Buttons = styled.button`
-  
+  display: flex;
+  justify-content: center;
+`
+export const ContainerIcons = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 15px);
+  gap: 30px;
+  justify-items: center;
+  justify-content: center;
+`
+export const StyleIcons = styled.a`
+  text-decoration: none;
+  padding: 2px;
+  transition: color 0.2s;
+  color: black;
+
+  :hover {
+    color: #78cc6d;
+  }
+`
+export const Span = styled.span`
+  padding: 0 5px;
+`
+export const Img = styled.img`
+  width: 100%;
+  border-radius: 4px 4px 0 0;
 `
